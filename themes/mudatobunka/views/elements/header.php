aaf4c78b2b506aad89844d0824f5c6614a949b88
三井 翔吾 <mitsui@queserser-j.co.jp>
2015-02-01 04:17:53 +0900

    グローバルのリンクを設定。

--- themes/mudatobunka/views/elements/header.php
+++ themes/mudatobunka/views/elements/header.php
@@ -8,8 +8,8 @@
   <ul class="gnav">
     <li><a href="<?php echo home_url('/about/'); ?>">ABOUT</a></li>
     <li><a href="<?php echo home_url(); ?>">ARCHIVE</a></li>
-    <li><a href="" target="_blank"><i class="fa fa-twitter" title="Twitter"></i></a></li>
-    <li><a href="" target="_blank"><i class="fa fa-instagram" title="Instagram"></i></a></li>
-    <li><a href="" target="_blank"><i class="fa fa-tumblr-square" title="Tumblr"></i></a></li>
+    <li><a href="https://twitter.com/todays_mitsui/" target="_blank"><i class="fa fa-twitter" title="Twitter"></i></a></li>
+    <li><a href="http://instagram.com/todays_mitsui/" target="_blank"><i class="fa fa-instagram" title="Instagram"></i></a></li>
+    <li><a href="http://todays-mitsui.tumblr.com" target="_blank"><i class="fa fa-tumblr-square" title="Tumblr"></i></a></li>
   </ul>
 </header>
