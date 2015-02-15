11d8f3ed3722e0bbb068e81d976ba541527221dc
三井 翔吾 <mitsui@queserser-j.co.jp>
2015-02-15 21:46:43 +0900

    ヘッダー・フッターにGitHubへのリンクを追加。

--- themes/mudatobunka/views/elements/header.php
+++ themes/mudatobunka/views/elements/header.php
@@ -8,6 +8,7 @@
   <ul class="gnav">
     <li><a href="<?php echo home_url('/about/'); ?>">ABOUT</a></li>
     <li><a href="<?php echo home_url(); ?>">ARCHIVE</a></li>
+    <li><a href="https://github.com/todays-mitsui" target="_blank"><i class="fa fa-github-square" title="GitHub"></i></a></li>
     <li><a href="https://twitter.com/todays_mitsui/" target="_blank"><i class="fa fa-twitter" title="Twitter"></i></a></li>
     <li><a href="http://instagram.com/todays_mitsui/" target="_blank"><i class="fa fa-instagram" title="Instagram"></i></a></li>
     <li><a href="http://todays-mitsui.tumblr.com" target="_blank"><i class="fa fa-tumblr-square" title="Tumblr"></i></a></li>
